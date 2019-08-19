11e1f65a77eecaa921cdd2cfbd62e24ba4a95262
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-19 09:29:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,7 +32,7 @@ monopoly_with_third_tier =
           :reading_railroad=>{},
           :pennsylvania_railroad=>{},
           :b_and_o_railroad=>{},
-          :shortline_railroad=> {} 
+          :shortline=> {} 
     
         }
       }
@@ -54,7 +54,7 @@ monopoly_with_fourth_tier=
           :reading_railroad=>{"mortgage_value"=>"$100"},
           :pennsylvania_railroad=>{"mortgage_value"=>"$200"},
           :b_and_o_railroad=>{"mortgage_value"=>"$400"},
-          :shortline_railroad=> {"mortgage_value"=>"$800"} 
+          :shortline=> {"mortgage_value"=>"$800"} 
     
         }
       }
